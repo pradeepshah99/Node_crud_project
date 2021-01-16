e818313e349f7f9ab0820811c364203cf2e25e8e
Pradeep Kumar Sahani <mark42virus>
2021-01-16 14:58:37 +0530

    addition

--- model/userSchema.js
+++ model/userSchema.js
@@ -24,6 +24,7 @@ user.pre('save', async function(next){
     try{
         const salt = await bcrypt.genSalt(10);
         const hasedPassword = await bcrypt.hash(this.password, salt);
+        this.password = hasedPassword;
 
         next();
     }
